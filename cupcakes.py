c7a571d06387b2caf852cff6fa577ebbfdbe545f
satvikag2001 <53266891+satvikag2001@users.noreply.github.com>
2019-07-27 19:47:40 +0530

    Add files via upload

--- cupcakes.py
+++ cupcakes.py
@@ -1,22 +1,16 @@
 number_of_cases = int(input())
 i = 0
-j = 1
+
 k = 0
-factors = []
-sorted_factors = []
+
+all_maxx=[]
 for i in range(number_of_cases):
 	number_of_cupcakes = int(input())
-	i += 1
-	while j <= number_of_cupcakes:
-		remainder = (number_of_cupcakes +1)%j
-		if remainder == 0:
-			factors.append(j)
-		else:
-			remainder = 0
-	sorted_factors=sorted(factors)
-	maxx = sorted_factors[-1]
-	all_maxx.append(max)
-	j += 1
+	if number_of_cupcakes%2 == 0:
+		maxx = (number_of_cupcakes + 2)/2
+	else:
+		maxx = (number_of_cupcakes +1)/2
+	all_maxx.append(maxx)
 for k in range(number_of_cases)	:
-	print("%d"%all_maxx[k])
+	print("%d"%(all_maxx[k]))
 	k += 1
