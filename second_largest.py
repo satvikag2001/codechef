61e240b5e85d89af16c8e3b0c108d8e8400797a5
satvikag2001 <53266891+satvikag2001@users.noreply.github.com>
2019-07-29 17:35:55 +0530

    Add files via upload

--- second_largest.py
+++ second_largest.py
@@ -1,5 +1,26 @@
 number_of_cases = int(input())
+i = 0
 for i in range(number_of_cases):
+	
 	inp = input().split()
-	inp = sorted(inp)
-	print(int(inp[1]))
+	a = int(inp[0])
+	b = int(inp[1])
+	c = int(inp[2])
+	if (a>b and a>c):
+		if b>c:
+			print(b)
+		else :
+			print(c)
+	elif (b>a and b>c):
+		if a>c:
+			print(a)
+		else :
+			print(c)
+	elif (c>b and c>a):
+		if b>a:
+			print(b)
+		else :
+			print(a)		
+	
+	i += 1
+	
